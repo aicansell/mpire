54e1b3b7b5104cdd30bb3770827f00f41e1c49a1
ayushchourasia01 <ayushchourasia01@gmail.com>
2023-11-05 22:49:53 +0530

    authentication

---
+++ authentication/serializers.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from authentication.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username','password', 'email', 'first_name', 'last_name', 'user_type', 'phone_number', 'state')
+        extra_kwargs = {
+            'password': {'write_only': True, 'required': True, 'validators': [validate_password]},
+            'email': {'required': True, 'validators': [UniqueValidator(queryset=User.objects.all())]},
+            'phone_number': {'required': True, 'validators': [UniqueValidator(queryset=User.objects.all())]},
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+            }
+        
+    def create(self, validated_data):
+        user_type = validated_data.get('user_type', 'end_consumer')
+        user = User.objects.create(
+            username = validated_data['username'],
+            email = validated_data['email'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
+            phone_number = validated_data['phone_number'],
+            user_type = user_type,
+            state = validated_data['state'],
+        )
+        
+        user.set_password(validated_data['password'])
+        user.save()
+        
+        return user
+    
+class LoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+            'password', 'last_login', 'is_active', 'is_superuser', 'is_staff',
+            'groups', 'user_permissions', 'date_joined',
+        )
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
