9e22750ebeef7fa69b177480000416dc70690c8d
ayushchourasia01 <ayushchourasia01@gmail.com>
2023-11-05 23:38:35 +0530

    model update

--- authentication/models.py
+++ authentication/models.py
@@ -13,9 +13,12 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
+        # Add a dummy 'username' argument with a default value
+        username = extra_fields.pop('username', 'admin')
+
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, password, username=username,**extra_fields)
     
 
 class User(AbstractUser):
