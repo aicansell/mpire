69c31a6dcac530945c7f18550a5d1c8607340e01
KRITGYA2001 <kritgyakumar92@gmail.com>
2023-11-18 18:39:50 +0530

    changes

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from authentication.models import User, EmailConfirmationToken
+
+admin.site.site_header = _('Mpire')
+admin.site.site_title = _('Admin')
+admin.site.index_title = _('Mpire Portal')  
 
 admin.site.register(User)
 admin.site.register(EmailConfirmationToken)
--- products/views.py
+++ products/views.py
@@ -6,10 +6,13 @@ from rest_framework.viewsets import ViewSet
 from rest_framework_tracking.mixins import LoggingMixin
 
 from products.models import Category, SubCategory, Product
-from products.serializers import CategorySerializer, SubCategorySerializer, ProductSerializer, ProductListSerializer
+from products.serializers import CategorySerializer, CategoryListSerializer
+from products.serializers import SubCategorySerializer, SubCategoryListSerializer
+from products.serializers import ProductSerializer, ProductListSerializer
 
 
-class CategoryViewSet(ViewSet,LoggingMixin):
+class CategoryViewSet(ViewSet, LoggingMixin):
+    
     def get_object(self, pk):
         return get_object_or_404(Category, pk=pk)
     
@@ -17,8 +20,61 @@ class CategoryViewSet(ViewSet,LoggingMixin):
         return Category.objects.all()
     
     def list(self, request):
-        serializer = CategorySerializer(self.get_queryset(), many=True)
+        serializer = CategoryListSerializer(self.get_queryset(), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk):
+        instance = self.get_object(pk)
+        serializer = CategoryListSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        request_data = {
+            'category_name': request.data.get('category_name'),
+        }
+        
+        try:
+            category = Category.objects.get(category_name__icontains=request_data['category_name'])
+            if category:
+                return Response({'status': 'error', 'message': 'Category already exists'}, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            serializer = CategorySerializer(data=request_data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            
+            response = {
+                'status': 'success',
+                'message': "Category created successfully",
+            }
+            
+            return Response(response, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk):
+        instance = self.get_object(pk)
+        
+        request_data = {
+            'category_name': request.data.get('category_name'),
+        }
+        
+        serializer = CategorySerializer(instance, data=request_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response = {
+            'status': 'success',
+            'message': "Category updated successfully",
+        }
+        
+        return Response(response, status=status.HTTP_201_CREATED)
+    
+    def destroy(self, request, pk):
+        instance = self.get_object(pk)
+        instance.delete()
+        response = {
+            'status': 'success',
+            'message': "Category deleted successfully",
+        }
+        
+        return Response(response, status=status.HTTP_204_NO_CONTENT)   
 
 class SubCategoryViewSet(ViewSet,LoggingMixin):
     def get_object(self, pk):
@@ -28,8 +84,64 @@ class SubCategoryViewSet(ViewSet,LoggingMixin):
         return SubCategory.objects.all()
     
     def list(self, request):
-        serializer = SubCategorySerializer(self.get_queryset(), many=True)
+        serializer = SubCategoryListSerializer(self.get_queryset(), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk):
+        instance = self.get_object(pk)
+        serializer = SubCategoryListSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        
+        request_data = {
+            'subcategory_name': request.data.get('subcategory_name'),
+            'category': request.data.get('category')
+        }
+        
+        try:
+            subcategory = SubCategory.objects.get(subcategory_name__icontains=request_data['subcategory_name'])
+            if subcategory:
+                return Response({'status': 'error', 'message': 'SubCategory already exists'}, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            serializer = SubCategorySerializer(data=request_data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            
+            response = {
+                'status': 'success',
+                'message': "SubCategory created successfully",
+            }
+            
+            return Response(response, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk):
+        instance = self.get_object(pk)
+        
+        request_data = {
+            'subcategory_name': request.data.get('subcategory_name'),
+            'category': request.data.get('category')
+        }
+        
+        serializer = SubCategorySerializer(instance, data=request_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response = {
+            'status': 'success',
+            'message': "SubCategory updated successfully",
+        }
+        
+        return Response(response, status=status.HTTP_201_CREATED)
+    
+    def destroy(self, request, pk):
+        instance = self.get_object(pk)
+        instance.delete()
+        response = {
+            'status': 'success',
+            'message': "SubCategory deleted successfully",
+        }
+        
+        return Response(response, status=status.HTTP_204_NO_CONTENT)
     
 class ProductViewSet(ViewSet,LoggingMixin):
 
@@ -47,4 +159,3 @@ class ProductViewSet(ViewSet,LoggingMixin):
         instance = self.get_object(pk)
         serializer = ProductListSerializer(instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
